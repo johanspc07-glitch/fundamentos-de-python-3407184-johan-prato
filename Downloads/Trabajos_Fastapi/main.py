a76d4d5eb2b1d24a0fcf99bf21270da86dd3b97b
johan prato <johanspc07@gmail.com>
2026-07-01 19:46:15 -0500

    creacion y separacion de carpeta modelos

--- Downloads/Trabajos_Fastapi/main.py
+++ Downloads/Trabajos_Fastapi/main.py
@@ -1,30 +1,26 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from modelos.clientes import Cliente
 
 
 app = FastAPI()
 
 lista_clientes: list[Cliente] = []
 
-class Cliente(BaseModel):
-    id: int
-    nombre: str
-    email: str
-    descripcion: str
 
 
-@app.get("/clientes")
+@app.get("/clientes", response_model=list[Cliente])
 def lista_clientes():
     return lista_clientes
 
-@app.get("/clientes/{cliente_id}")
+@app.get("/clientes/{cliente_id}", response_model=Cliente)
 def lista_cliente(cliente_id: int):
     for i, obj_cliente in enumerate(lista_clientes):
         if obj_cliente.get("id") == cliente_id:
             return obj_cliente
 
-@app.post("/clientes")
-def crear_cliente(datos_cliente: Cliente):
-    lista_clientes.append(datos_cliente)
-    return datos_cliente
+@app.post("/clientes", response_model=Cliente)
+def crear_cliente(datos_cliente: ClienteCrear):
+    cliente_val = Cliente.model_validate(datos_cliente.model_dump())
+    lista_clientes.append(cliente_val)
+    return cliente_val
     
